25659400665bf70642c8539c4c1b8cecd3471854
Darius Strasel <strasel.darius@gmail.com>
2017-03-05 22:31:40 -0500

    Added event listener for select onchange() and callback function for pyramid symbol, getSymbol().

--- build/js/main.js
+++ build/js/main.js
@@ -5,35 +5,56 @@
  * Determines the current value that the user has typed in the 'How high?' text-box,
  * and then draws a pyramid with that height.
  */
+
 function determineHeightAndThenDrawPyramid() {
 
     // just so we know we're here
     console.log("someone invoked the determineHeightAndThenDrawPyramid function!");
 
-    // TODO 3
     // figure out the height the user typed (replace the "5" below)
     var userHeight = document.getElementById("height").value;
+
+    // Update height label
+    updateHeightLabel();
+
     heightStr = userHeight;
 
     // here we convert the string to an int
     height = parseInt(heightStr);
     // TODO 2
     // draw the pyramid with the given height
-    drawPyramid(height);
+    drawPyramid(height, getSymbol());
 }
 
+function updateHeightLabel(height) {
+    if (height === undefined) {
+        var height_value = document.getElementById("height").value;
+    }
+    else {
+        var height_value = height;
+    }
+    var label = document.getElementById("label_height");
+    return label.textContent = height_value;
+}
 
 // TODO 1
 // hook up the button's click event to our determineHeightAndThenDrawPyramid function
-var drawButton_element = document.getElementById("drawButton");
-var eventHandler = drawButton_element.addEventListener("click", determineHeightAndThenDrawPyramid);
-
+var symbolSelector_element = document.getElementById("symbol_selector");
+var eventHandler = symbolSelector_element.addEventListener("change", determineHeightAndThenDrawPyramid);
 /**
  * drawPyramid
  *
  * Renders, in the HTML document, a Mario pyramid of the specified height
  */
- function drawPyramid(height) {
+
+function getSymbol() {
+    var symbol_element = document.getElementById("symbol_selector");
+    var symbolOptions_collection = symbol_element.options;
+    var selected_element = symbolOptions_collection.selectedIndex;
+    return symbolOptions_collection[selected_element].value;
+}
+
+ function drawPyramid(height, symbol) {
 
      // TODO 4
      // before drawing, clear the old content
@@ -56,7 +77,7 @@ var eventHandler = drawButton_element.addEventListener("click", determineHeightA
              rowStr += ".";
          }
          for (var i = 0; i < numBricks; i++) {
-             rowStr += "#";
+             rowStr += symbol;
          }
 
         // create a text element with the string of characters
@@ -70,3 +91,9 @@ var eventHandler = drawButton_element.addEventListener("click", determineHeightA
         document.getElementById("pyramid").appendChild(rowElem);
     }
 }
+
+    // Add function and event listener for drawing pyramid oninput change
+var slider_element = document.getElementById("height");
+var slider_eventListener = slider_element.addEventListener("input", determineHeightAndThenDrawPyramid);
+
+    //TODO: Add function for modifying slider label.
